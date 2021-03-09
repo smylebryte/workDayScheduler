598b484bf92973e8af4574a36fb8c7eecb35d856
Smylebryte <aliciahughes1446@gmail.com>
2021-03-08 22:50:11 -0600

    added code to update logic for color coding timeblocks

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# workDayScheduler
+In this project, I created a calendar application that allows the user to save events for each hour of the day.  This app uses dynamically updated HTML and CSS powered by jQuery in order to display color coded timeblocks that indicate whether a timeblock is in the past, present or future.
+
+![Workday Scheduler](Scheduler Pic.jpg)
+
--- script.js
+++ script.js
@@ -1,31 +1,52 @@
 var today = moment();
 var saveBtn = $(".saveBtn");
-var description = $(".description");
 
-description = localStorage.getItem("workDayText");
+// Load all descriptions
+$(".description").each(function(i, elem) {
+    let storageId = $(elem).parent().attr("id");
+    let descriptionText = localStorage.getItem(storageId) || "";
+    $(elem).val(descriptionText);
+});
+
+function updateColor() {
+    let currTime = parseInt(moment().format("HH"));
+    
+    $(".time-block").each(function(i, elem) {
+        let currDiv = $(elem);
+        currDiv.remove("past");
+        currDiv.remove("future");
+        currDiv.remove("present");
+
+        let divId = currDiv.attr("id");
+        let divHour = parseInt(divId.replace("hour", ""));
+
+        if (divHour > currTime) {
+            currDiv.addClass("future");
+        }
+
+        if (divHour == currTime) {
+            currDiv.addClass("present");
+        }
+
+        if (divHour < currTime) {
+            currDiv.addClass("past");
+        }
+
+    });
+}
+
+setInterval(updateColor, 10000);
 
 $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
 
 saveBtn.on('click', function(event) {
     event.preventDefault();
-    // alert ("hey!");
-    // description.textContent = description;
-    localStorage.setItem("workDayText", description);
+    let buttonThatWasClicked = $(this);
+    let buttonId = buttonThatWasClicked.attr("id");
+    let textId = buttonId.replace("save-", "");
+
+    let $text = $("#" + textId + " .description");
+
+    localStorage.setItem(textId, $text.val());
 })
-
-$(".time-block").each(function(i, elem) {
-    let currHour = moment().format("HH");
-    let elemId = elem.id;
-    let idHour = elemId.replace("hour");
-
-    if (currHour <= idHour) {
-        $("#elemId").addClass("past");
-    } else if (currHour = idHour) {
-        $("#elemId").addClass("present");
-    } else {
-    $("#elemId").addClass("future");
-}
-})
-
-
 
